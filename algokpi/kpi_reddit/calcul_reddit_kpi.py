f534021d2742a5c030a1d97540663c865b077472
csacenda <cysacenda@gmail.com>
2018-01-23 14:59:13 +0100

    -Auto install with setup.py -working on calcul kpi

--- algokpi/kpi_reddit/calcul_reddit_kpi.py
+++ algokpi/kpi_reddit/calcul_reddit_kpi.py
@@ -10,7 +10,11 @@ conf = Config()
 
 # % increase
 def calcul_kpi_subscribers_trend():
-    toto = 0
+    logging.warning("calcul_kpi_subscribers_trend - start")
+
+    __calcul_kpi_subscribers_30d_trend(172091) #Raiblocks
+
+    logging.warning("calcul_kpi_subscribers_trend - end")
 
 def __calcul_kpi_subscribers_1d_trend():
     toto = 0
@@ -25,16 +29,26 @@ def __calcul_kpi_subscribers_15d_trend():
     toto = 0
 
 def __calcul_kpi_subscribers_30d_trend(coin_id):
-    squery = __create_query_subscribers_30d_trend()
+    squery = __create_query_subscribers_30d_trend(coin_id)
+    dbconn = DbConnection()
+    rows = dbconn.get_query_result(squery)
+
+    # if(rows.count)
+    #if we get information from db
+    if(len(rows) == 2):
+        #increase = (ValueAfter-ValueBefore)/(ValueBefore)
+        increase = (rows[1][1] - rows[0][1]) / (rows[0][1])
 
 def __create_query_subscribers_30d_trend(coin_id):
-    squery = '(select * from social_stats_reddit\n'
+
+    # Get today's infos + infos from 30d ago
+    squery = '(select "IdCoinCryptoCompare", "Reddit_subscribers", "Reddit_active_users", "timestamp" from social_stats_reddit\n'
     squery += 'where "IdCoinCryptoCompare" = ' + "'" + str(coin_id) + "'\n"
     squery += "and timestamp > CURRENT_TIMESTAMP - interval '30 days'\n"
     squery += 'order by timestamp asc\n'
     squery += 'LIMIT 1)\n'
     squery += 'UNION ALL\n'
-    squery += '(select * from social_stats_reddit\n'
+    squery += '(select "IdCoinCryptoCompare", "Reddit_subscribers", "Reddit_active_users", "timestamp" from social_stats_reddit\n'
     squery += 'where "IdCoinCryptoCompare" = ' + "'" + str(coin_id) + "'\n"
     squery += "and timestamp > CURRENT_TIMESTAMP - interval '30 days'\n"
     squery += 'order by timestamp desc\n'
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='algocryptos',
+    version='0.0.1',
+    packages=['', 'dataimporter/utils', 'dataimporter/config', 'dataimporter/reddit', 'dataimporter/dbaccess', 'dataimporter/extractdata', 'dataimporter/coinmarketcap', 'dataimporter/cryptocompare'],
+    package_dir={'algokpi': 'algokpi', 'dataimporter': 'dataimporter'},
+    install_requires=['requests', 'logging', 'configparser', 'ratelimit', 'psycopg2', 'tzlocal'],
+    url='',
+    license='',
+    author='csacenda',
+    author_email='',
+    description=''
+)
