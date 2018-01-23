f73c36bbaacb97578251c88e753fad560da62f88
csacenda <cysacenda@gmail.com>
2018-01-23 15:03:28 +0100

    Correct bug setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 
+#packages=['', 'dataimporter/utils', 'dataimporter/config', 'dataimporter/reddit', 'dataimporter/dbaccess', 'dataimporter/extractdata', 'dataimporter/coinmarketcap', 'dataimporter/cryptocompare'],
+
+
 setup(
     name='algocryptos',
     version='0.0.1',
-    packages=['', 'dataimporter/utils', 'dataimporter/config', 'dataimporter/reddit', 'dataimporter/dbaccess', 'dataimporter/extractdata', 'dataimporter/coinmarketcap', 'dataimporter/cryptocompare'],
+packages=['', 'dataimporter','algokpi'],
     package_dir={'algokpi': 'algokpi', 'dataimporter': 'dataimporter'},
     install_requires=['requests', 'logging', 'configparser', 'ratelimit', 'psycopg2', 'tzlocal'],
     url='',
