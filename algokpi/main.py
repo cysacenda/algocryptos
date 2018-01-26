bf09e89d207dd2b9de78b08ea0661189cc116c99
cysacenda <cysacenda@gmail.com>
2018-01-26 10:29:21 +0100

    -AlgoKPI : add parameters for main -Cron : schedule algokpi

--- algokpi/main.py
+++ algokpi/main.py
@@ -1,6 +1,7 @@
 import logging
 from config.config import Config
 from kpi_reddit import calcul_reddit_kpi
+import argparse
 
 #Configuration
 conf = Config()
@@ -11,9 +12,14 @@ logging.basicConfig(filename='algokpi.log',
 
 logging.warning("Started")
 
-# TODO : Mettre en place parsing des arguments (cf. main de dataimporter)
-calcul_reddit_kpi.calcul_kpi_subscribers_trend()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Outil permettant l'analyse des données marchés et réseaux sociaux pour les cryptocurrencies")
+    parser.add_argument('-r', '--reddit', dest="reddit", help='Calcul KPIs related to Reddit',
+                        action='store_true')
+    args = parser.parse_args()
 
+    if (args.reddit):
+        calcul_reddit_kpi.calcul_kpi_subscribers_trend()
 
 logging.warning("Stopped")
 
