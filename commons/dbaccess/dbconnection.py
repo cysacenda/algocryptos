88c6358ba9179cad15375fcc778bcf34b2d75fad
cysacenda <cysacenda@gmail.com>
2018-02-04 22:39:11 +0100

    move commons module to special folders /commons to avoir problems on linux server on AWS

---
+++ commons/dbaccess/dbconnection.py
@@ -0,0 +1,43 @@
+import psycopg2
+from commons.config import Config
+import logging
+
+
+class DbConnection:
+    conn = None
+    conf = None
+
+    def __init__(self):
+        self.conf = Config()
+        dbhost = self.conf.get_config('db', 'dbhost')
+        dbname = self.conf.get_config('db', 'dbname')
+        dbuser = self.conf.get_config('db', 'dbuser')
+        dbpassword = self.conf.get_config('db', 'dbpassword')
+
+        connstring = "host='" + dbhost + "' dbname='" + dbname + "' user='" + dbuser \
+                     + "' password='" + dbpassword + "'"
+        try:
+            self.conn = psycopg2.connect(connstring)
+        except Exception as e:
+            logging.error("Error : " + str(e) + " - " + "Error while connecting to DB : " + connstring)
+
+    def get_query_result(self, query):
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute(query)
+        except Exception as e:
+            logging.error("Error : " + str(e) + " - " + "Error while executing query : " + query)
+            return None
+
+        rows = cursor.fetchall()
+        return rows
+
+    def exexute_query(self, query):
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute(query)
+            self.conn.commit()
+            return 0
+        except Exception as e:
+            logging.error("Error : " + str(e) + " - " + "Error while executing query : " + query)
+            return -1
