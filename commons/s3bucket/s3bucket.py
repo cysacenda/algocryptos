386ee29b5adb88bbd049b63bdd934e074821783b
cysacenda <cysacenda@gmail.com>
2018-03-18 22:37:11 +0100

    debug

--- commons/s3bucket/s3bucket.py
+++ commons/s3bucket/s3bucket.py
@@ -3,6 +3,7 @@ from boto3.s3.transfer import S3Transfer
 import boto3
 import os
 import utils
+import logging
 
 conf = Config()
 access_key = conf.get_config('s3_bucket', 'access_key')
@@ -13,10 +14,12 @@ client = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=
 transfer = S3Transfer(client)
 
 def transfer_folder_content_to_s3(local_path, s3_path):
+    logging.warning("transfer_folder_content_to_s3 - start")
     os.chdir(utils.get_path_for_system_spe(os.getcwd(), local_path))
     for root, dirs, files in os.walk(".", topdown=False):
         for local_filename in files:
             transfer_file_to_s3(local_filename, s3_path)
+    logging.warning("transfer_folder_content_to_s3 - end")
 
 def transfer_file_to_s3(local_filename, s3_path):
     transfer.upload_file(local_filename, bucket_name, s3_path + '/' + local_filename)
