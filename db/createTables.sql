f6c2e51a64ba797cb39aec827ed78bafbb7a8363
achollet <arnaud.chollet@gmail.com>
2018-04-04 23:30:43 +0200

    Add table KPI for google trend

--- db/createTables.sql
+++ db/createTables.sql
@@ -663,3 +663,29 @@ GRANT ALL ON TABLE public.kpi_global_data_histo TO postgres;
 
 COMMENT ON TABLE public.kpi_global_data_histo
     IS 'Contains calculated kpi on global data (historical table).';
+
+
+
+-- Table: public.kpi_googletrend
+
+-- DROP TABLE public.kpi_googletrend;
+CREATE TABLE public.kpi_googletrend
+(
+    id_cryptocompare bigint,
+    search_1d_trend double precision,
+    search_3d_trend double precision,
+    search_7d_trend double precision,
+    search_15d_trend double precision,
+    search_1m_trend double precision,
+    timestamp timestamp with time zone default current_timestamp
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.kpi_googletrend
+    OWNER to postgres;
+
+GRANT ALL ON TABLE public.kpi_googletrend TO dbuser;
+GRANT ALL ON TABLE public.kpi_googletrend TO postgres;
