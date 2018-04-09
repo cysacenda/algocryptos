bcd15fad6a586f064a90c8526383dcc4b23d77aa
csacenda <cysacenda@gmail.com>
2018-04-09 17:33:46 +0200

    working on slack alertes

---
+++ commons/slack/slack.py
@@ -0,0 +1,18 @@
+from commons.config import Config
+import logging
+import os
+from slackclient import SlackClient
+
+conf = Config()
+slack_token = conf.get_config('slack', 'slack_api_token')
+sc = SlackClient(slack_token)
+
+def post_message(channel_name, message_content):
+    sc.api_call(
+        "chat.postMessage",
+        channel=channel_name,
+        text=message_content
+    )
+
+def post_message_to_bot_alert(message):
+    post_message('bot_alerts', message)
