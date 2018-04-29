5036998991d22a9504824dab8ba99881947d7e3d
cysacenda <cysacenda@gmail.com>
2018-04-29 12:42:39 +0200

    update modifDB for deploy

--- db/modifsBDD.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.alert_type(
-	id_alert_type, global_alert, category_type, filter_type,  trigger_period_hour, description)
-	VALUES (2, false, 'volume', 'TOP100', '1', '[Crypto Top 100][Mean vol. 1h/30d > 800%] #crypto_name# (#crypto_symbol#): #val1_double#%');
