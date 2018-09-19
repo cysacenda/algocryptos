755128c8b0dc5e095d12f3493224094dfd0c58b1
cysacenda <cysacenda@gmail.com>
2018-09-19 11:55:20 +0200

    Working on ML + add SQL table for data histo

--- db/createTables.sql
+++ db/createTables.sql
@@ -851,3 +851,33 @@ ALTER TABLE public.alert_type
 
 GRANT ALL ON TABLE public.alert_type TO dbuser;
 GRANT ALL ON TABLE public.alert_type TO postgres;
+
+
+-- Table: public.histo_ohlcv_old
+
+-- DROP TABLE public.histo_ohlcv_old;
+
+CREATE TABLE public.histo_ohlcv_old
+(
+    id_cryptocompare bigint NOT NULL,
+    open_price double precision,
+    high_price double precision,
+    low_price double precision,
+    close_price double precision,
+    volume_crypto double precision,
+    volume_usd double precision,
+    timestamp timestamp with time zone
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.histo_ohlcv_old
+    OWNER to postgres;
+
+GRANT ALL ON TABLE public.histo_ohlcv_old TO dbuser;
+GRANT ALL ON TABLE public.histo_ohlcv_old TO postgres;
+
+COMMENT ON TABLE public.histo_ohlcv_old
+    IS 'Contains one line per cryptocurrency per day with informations on OHLC and the volumes of the cryptocurrency, data comes from CryptoCompare and volumes are USD only. The goal is to have data for technical analysis before Dec 2017';
