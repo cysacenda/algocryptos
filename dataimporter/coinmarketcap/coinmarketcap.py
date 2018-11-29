9470a29446eb09f91f5b873f3dd79d1ef540e579
cysacenda <cysacenda@gmail.com>
2018-11-29 12:46:23 +0100

    Updating CoinmarketCapAPI => CoinMarketCapAPI Pro

--- dataimporter/coinmarketcap/coinmarketcap.py
+++ dataimporter/coinmarketcap/coinmarketcap.py
@@ -1,6 +1,8 @@
 import requests
 import logging
 from commons.config import Config
+import pandas as pd
+import time, requests, json
 
 
 class CoinMarketCap:
@@ -57,5 +59,43 @@ class CoinMarketCap:
             return ','.join(parameter)
         else:
             return parameter
+
+    # endregion
+
+
+class CoinMarketCapNew:
+    conf = None
+    URL_PRICE_LIST = None
+    API_KEY = None
+
+    def __init__(self):
+        self.conf = Config()
+
+        # API urls
+        self.URL_PRICE_LIST = self.conf.get_config('cmc_params_new', 'url_prices')
+        self.API_KEY = self.conf.get_config('cmc_params_new', 'api_key')
+
+    # region Get prices
+    def get_price_list(self):
+        return self.get_dataframe_from_response(self.URL_PRICE_LIST)
+
+    def get_dataframe_from_response(self, url):
+        df = pd.DataFrame()
+        try:
+            headers = {
+                'Accept': 'application/json',
+                'Accept-Encoding': 'deflate, gzip',
+                'X-CMC_PRO_API_KEY': self.API_KEY,
+            }
+            r = requests.get(url, headers=headers)
+            if r.status_code == 200:
+                response = json.loads(r.text)
+                df = pd.DataFrame(response['data'])
+            else:
+                logging.error("Error getting prices information from CMC. Response status_code= " + str(r.status_code))
+                logging.error("Message= " + str(r.text))
+        except Exception as e:
+            logging.error("Error getting prices information from CMC. " + str(e))
+        return df
 
     # endregion
