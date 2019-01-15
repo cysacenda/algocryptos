9d86fb26d55e54efaee66368090bca0c39271af6
cysacenda <cysacenda@gmail.com>
2019-01-15 10:23:42 +0100

    Working on ML / Trading: SIMULATION MODE FOR DEPLOY

--- algo/trading/backtesting_strategy.py
+++ algo/trading/backtesting_strategy.py
@@ -17,7 +17,7 @@ class BacktestingStrategy:
         self.param_bet_size = float(conf.get_config('trading_module_params', 'bet_size'))  # %
         self.param_min_bet_size = float(conf.get_config('trading_module_params', 'min_bet_size'))  # $
         self.param_pct_order_placed = float(conf.get_config('trading_module_params', 'pct_order_placed'))  # 1% up/down
-        self.param_nb_periods_to_hold_position = int(conf.get_config('trading_module_params', 'pct_order_placed'))  # 1d
+        self.param_nb_periods_to_hold_position = int(conf.get_config('trading_module_params', 'nb_periods_to_hold_position'))  # 1d
         self.signals = {}
         self.all_signals = {}
 
